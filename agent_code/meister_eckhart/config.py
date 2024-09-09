ac0ee290b6c6f6cf752313e8c819469258a372ba
prio <prioteasa.andrei77@gmail.com>
2024-09-09 19:19:31 +0300

    forgot config file

---
+++ agent_code/meister_eckhart/config.py
@@ -0,0 +1,38 @@
+import torch
+import math
+import random
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import deque, namedtuple
+from torch.utils.data import Dataset
+from tqdm import tqdm
+
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
+
+TRAIN_DEVICE = torch.device(
+    "cuda" if torch.cuda.is_available() else
+    "mps" if torch.backends.mps.is_available() else
+    "cpu"
+)
+LEARNING_RATE = 0.0001
+MODEL_SAVE_PATH = "saved_models/my-saved-model-rule-based-coin-heaven.pt"
+
+# Hyper parameters -- DO modify
+RECORD_ENEMY_TRANSITIONS = 1.0 # record enemy transitions with probability ...
+GAMMA = 0.99
+MEMORY_SIZE = 10000
+BATCH_SIZE = 64
+
+# the number of training steps
+TRAIN_EPOCHS = 10_000
+ROUND_TO_PLOT = 5000 #default 200
+SAVE_MODEL_EVERY = 1000
+UPDATE_TARGET_EVERY = 10
+
+SCENARIO = "coin_heaven"
+SAVED_TRANISION_PATH = 'rule_based_transitions/rule_based_training_memory_' + SCENARIO + '.pt'
