7b44fb1ada53283b08d4b41a153a9db510ccc770
Prioteasa Cristi Andrei <57443744+PrioteasaAndrei@users.noreply.github.com>
2024-09-23 22:14:51 +0200

    Update model.py

--- agent_code/meister_eckhart_imitation/model.py
+++ agent_code/meister_eckhart_imitation/model.py
@@ -7,10 +7,7 @@ import torch.nn.functional as F
 from collections import deque, namedtuple
 from torch.utils.data import Dataset
 from tqdm import tqdm
-
-
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
+from .config import *
 
 
 class ReplayMemory():
@@ -29,20 +26,28 @@ class ReplayMemory():
     def can_provide_sample(self, sample_size: int):
         return len(self.memory) >= sample_size
 
+def create_model(input_shape, num_actions, logger, model_type: str):
+    if model_type == "JointDQN":
+        return JointDQN(input_shape=input_shape, num_actions=num_actions, logger=logger)
+    elif model_type == "SimpleDQN":
+        return SimpleDQN(input_shape=input_shape, num_actions=num_actions, logger=logger)
+    else:
+        raise ValueError("Invalid model type {}".format(model_type))
 
-class JointDQN(nn.Module):
-    def __init__(self,input_shape=(8,17,17),num_actions=6,logger=None):
+class SimpleDQN(nn.Module):
+    '''
+    Number of parameters: 384 + input_shape_vectorized * 64
+    '''
+    def __init__(self,input_shape=(8,7,7),num_actions=6,logger=None):
         super(JointDQN, self).__init__()
 
         self.vectorized_shape = math.prod(input_shape)
        
         self.net = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(self.vectorized_shape, 128),
+            nn.Linear(self.vectorized_shape, 64),
             nn.ReLU(),
-            nn.Linear(128, 128),
-            nn.ReLU(),
-            nn.Linear(128, num_actions),
+            nn.Linear(64, num_actions),
             nn.Softmax(dim=1)
         )
 
@@ -54,69 +59,77 @@ class JointDQN(nn.Module):
         
     
 
-# class JointDQN(nn.Module):
-#     '''
-#     Modify the kernel size and stride to obtain the desired feature space size
-#     '''
-#     def __init__(self,input_shape=(8,17,17),num_actions=6,logger=None):
-#         super(JointDQN, self).__init__()
+class JointDQN(nn.Module):
+    '''
+    Network dimensions:
+    input: 8x17x17
+    feature_space: 128
+    total number of parameters in the feature extractor: 5808 
+    total number of parameters in the dqn: 8646 
+    total params: 14454
+
+    NOTE: maverick has only 1740 parameters
+
+    '''
+    def __init__(self,input_shape=(8,7,7),num_actions=6,logger=None):
+        super(JointDQN, self).__init__()
         
-#         self.feature_extractor = nn.Sequential(
-#             nn.Conv2d(in_channels=input_shape[0], out_channels=16, kernel_size=3),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size=2),  # Pooling to reduce spatial dimensions
-#             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size=2),  # Pooling to reduce spatial dimensions
-#             nn.Flatten()
-#         )
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape[0], out_channels=16, kernel_size=3),
+            nn.ReLU(),
+            # nn.MaxPool2d(kernel_size=2),  # Pooling to reduce spatial dimensions
+            # nn.Conv2d(in_channels=16, out_channels=32, kernel_size=2),
+            # nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),  # Pooling to reduce spatial dimensions
+            nn.Flatten()
+        )
 
-#         self.logger = logger
+        self.logger = logger
 
-#         self.dqn_input_size = self.feature_size(input_shape)
+        self.dqn_input_size = self.feature_size(input_shape)
 
-#         self.dqn = nn.Sequential(
-#             nn.Linear(self.dqn_input_size, 64),
-#             nn.ReLU(),
-#             nn.Linear(64, num_actions),
-#             nn.Softmax(dim=1) 
-#         )
+        self.dqn = nn.Sequential(
+            nn.Linear(self.dqn_input_size, 64),
+            nn.ReLU(),
+            nn.Linear(64, num_actions),
+            nn.Softmax(dim=1) 
+        )
         
-#     def forward(self, x):
-#         features = self.feature_extractor(x)
-#         # self.logger.info(f"Features shape: {features.shape}")
-#         action_distr = self.dqn(features)
-#         # self.logger.info(f"Action distribution shape: {action_distr.shape}")
-#         return action_distr
+    def forward(self, x):
+        features = self.feature_extractor(x)
+        self.logger.info(f"Features shape in model: {features.shape}")
+        action_distr = self.dqn(features)
+        return action_distr
     
-#     def feature_size(self, input_shape):
-#         # Helper function to calculate the flattened size of CNN output
-#         with torch.no_grad():
-#             x = torch.zeros(1, *input_shape)
-#             x = self.feature_extractor(x)
-#             return x.view(1, -1).size(1)
+    def feature_size(self, input_shape):
+        # Helper function to calculate the flattened size of CNN output
+        with torch.no_grad():
+            x = torch.zeros(1, *input_shape)
+            
+            x = self.feature_extractor(x)
+            self.logger.info(f"Input shape: {x.shape}")
+            return x.view(1, -1).size(1)
         
-#     def number_of_params(self):
-#         return sum(p.numel() for p in self.parameters() if p.requires_grad)
+    def number_of_params(self):
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
         
 def action_to_tensor(action: str):
     '''
     Convert the action string to a tensor
     '''
-    if action == 'UP':
-        return torch.tensor([0])
-    elif action == 'RIGHT':
-        return torch.tensor([1])
-    elif action == 'DOWN':
-        return torch.tensor([2])
-    elif action == 'LEFT':
-        return torch.tensor([3])
-    elif action == 'WAIT':
-        return torch.tensor([4])
-    elif action == 'BOMB':
-        return torch.tensor([5])
-    else:
-        raise ValueError("Invalid action string {}".format(action))
+
+    ## get index of action in ACTION and return it as tensor
+
+    try:
+        # Find the index of the target in the list
+        index = ACTIONS.index(action)
+    except ValueError:
+        # If the target is not found, handle the exception (optional)
+        raise ValueError(f"The target '{action}' is not found in the list.")
+    
+    # Convert the index to a tensor
+    index_tensor = torch.tensor(index)
+    return index_tensor
 
 # This function is based on the following tutorial: https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
 def train_step(self, batch_size: int, gamma: int, device: torch.device, memory: ReplayMemory):
@@ -160,8 +173,14 @@ def train_step(self, batch_size: int, gamma: int, device: torch.device, memory: 
     # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
     # columns of actions taken. These are the actions which would've been taken
     # for each batch state according to policy_net
-    state_action_values = self.policy_net(state_batch.float().to(device)).gather(1, action_batch.to(device))
 
+    self.logger.info(f"shape of state_batch: {state_batch.size()}")
+    #when doing a training step action_batch has not the correct size. TODO:CHECK IF CORRECT
+    action_batch = action_batch[:, None]
+    self.logger.info(f"shape of action_batch: {action_batch.size()}")
+    self.logger.info(f"Calculating policy net")
+    state_action_values = self.policy_net(state_batch.float().to(device)).gather(1, action_batch.to(device))
+    self.logger.info(f"Calculating target net")
     # Compute V(s_{t+1}) for all next states.
     # Expected values of actions for non_final_next_states are computed based
     # on the "older" target_net; selecting their best reward with max(1).values
@@ -170,6 +189,8 @@ def train_step(self, batch_size: int, gamma: int, device: torch.device, memory: 
     # Reason for using the target network : https://stackoverflow.com/questions/54237327/why-is-a-target-network-required
     next_state_values = torch.zeros(batch_size, device=device)
     with torch.no_grad():
+        self.logger.info(f"non_final_next_states: {non_final_next_states.shape}")
+        self.logger.info(f"non_final_mask: {non_final_mask.shape}")
         next_state_values[non_final_mask] = self.target_net(non_final_next_states.float().to(device)).max(1).values
     # Compute the expected Q values
     expected_state_action_values = (next_state_values.view(-1, 1) * gamma) + reward_batch.to(device)
